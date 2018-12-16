701fb8297f3f6fb57feee49d9e40f895f552b3ee
loghinalexandru <loghinalexandru61@gmail.com>
2018-12-16 12:28:04 +0200

    CodeDocumentation/Refactoring

--- flappybird_agent.py
+++ flappybird_agent.py
@@ -11,12 +11,27 @@ eps = 0.1
 moves = [0, 119]
 
 def get_move(model, state):
+	'''
+	Function: get_move
+	Summary: Get Random/Model move
+	Attributes: 
+		@param (model):Built Model
+		@param (state):Selected Features
+	Returns: [0,1]
+	'''
 	if np.random.random() < eps:
 		return np.random.choice([0, 1])
 	else:
 		return np.argmax(model.predict(state).reshape(2,))
 
 def get_features(state):
+	"""
+	Function: get_features
+	Summary: Getting NN Features
+	Attributes: 
+		@param (state): Game State Info
+	Returns: Selected Features
+	"""
 	features_list = []
 	features_list.append(state["player_vel"])
 	features_list.append(state["player_y"] -(state["next_pipe_top_y"] + state["next_pipe_bottom_y"]) / 2) # Default pipe gap size is 100
@@ -26,18 +41,21 @@ def get_features(state):
 	return np.array(features_list).reshape(1,5)
 
 def build_model():
+	'''
+	Function: build_model
+	Summary: NN Model
+	Returns: Built Model
+	'''
 	model = Sequential()
-	model.add(InputLayer((5,))) # player_y , velocity , distance from player to pipe gap ,
+	model.add(InputLayer((5,)))
 	model.add(Dense(50, activation="sigmoid"))
 	model.add(Dense(50 , activation="relu"))
 	model.add(Dense(2 , activation= "linear"))
 	model.compile(keras.optimizers.Adam(), loss = "mse", metrics = ["accuracy"])
 	return model
 
-
 def save_model(model):
 	model.save_weights("weights.h5")
-
 
 def load_model(model):
 	model.load_weights("weights.h5")
@@ -45,6 +63,14 @@ def load_model(model):
 	return model
 
 def update_model(model, batch):
+	'''
+	Function: update_model
+	Summary: Q-Learning Algorithm
+	Attributes: 
+		@param (model): Built Mode
+		@param (batch): Train Data
+	Returns: None
+	'''
 	gamma = 0.95
 	for features, next_features, index, reward , game_over in batch:
 		y = model.predict(features)
@@ -56,11 +82,20 @@ def update_model(model, batch):
 		model.fit(features, y, batch_size = 32, epochs = 1 , verbose=False)
 
 def train(env, model, game):
+	'''
+	Function: train
+	Summary: Main Game Loop
+	Attributes: 
+		@param (env): PLE Game Enviorement
+		@param (model): Built Model
+		@param (game): Selected Game (FlappyBird)
+	Returns: None
+	'''
 	global eps
 	epoch = 0
 	total_reward = 5
 	experience = []
-	while total_reward < 100:
+	while total_reward < 300:
 		game_over = env.game_over()
 		features = get_features(game.getGameState())
 		index = get_move(model, features)
@@ -69,7 +104,6 @@ def train(env, model, game):
 		experience.append((features, get_features(game.getGameState()), index, reward , game_over))
 		if len(experience) > 10000:
 			del experience[0]
-
 		if env.game_over():
 			print("Training epoch: " , epoch , " Reward :" , total_reward)
 			epoch += 1
@@ -78,8 +112,6 @@ def train(env, model, game):
 				update_model(model, random.sample(experience, 32))
 			env.reset_game()
 			eps = max(eps - 0.001, 0.01)
-
-	save_model(model)
 
 if __name__ == "__main__":
 	total_reward = 0
